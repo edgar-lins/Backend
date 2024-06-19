ac8edfdf6cd1cec798f5b7a7c0d14c4d3e696b8b
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-19 10:46:53 -0300

    Class: Applying innerJoin

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -64,14 +64,21 @@ class NotesController {
             const filterTags = tags.split(',').map(tag => tag.trim());
 
             notes = await knex("tags")
-            .whereIn("name", filterTags)
+                .select([
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id",
+                ])
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("notes", "notes.id", "tags.note_id")
         } else {
             notes = await knex("notes")
-            .where({ user_id })
-            .whereLike("title", `%${title}%`)
-            .orderBy("title")
+                .where({ user_id })
+                .whereLike("title", `%${title}%`)
+                .orderBy("title");
         }
-
 
         return response.json(notes);
     }
