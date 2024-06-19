207fc06dda32f9a7c4f0fddea84767b5a3072923
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-19 11:26:02 -0300

    Class: Getting tags from notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -73,6 +73,7 @@ class NotesController {
                 .whereLike("notes.title", `%${title}%`)
                 .whereIn("name", filterTags)
                 .innerJoin("notes", "notes.id", "tags.note_id")
+                .orderBy("notes.title")
         } else {
             notes = await knex("notes")
                 .where({ user_id })
@@ -80,7 +81,17 @@ class NotesController {
                 .orderBy("title");
         }
 
-        return response.json(notes);
+        const userTags = await knex("tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
+        return response.json(notesWithTags);
     }
 }
 
